90f6332bf86e2e2c3ffefe99bcc2a1204f0e3bf8
Iván Martínez <ivanmartinez33@outlook.com>
2023-03-01 11:46:53 -0600

    script now updates notification service

--- whitelabel.js
+++ whitelabel.js
@@ -5,12 +5,14 @@ const plist = require('plist');
 // Files paths
 const ROCKET_CHAT_INFO_PLIST_PATH = './ios/RocketChatRN/Info.plist';
 const SHARE_ROCKET_CHAT_INFO_PLIST_PATH = './ios/ShareRocketChatRN/Info.plist';
+const NOTIFICATION_SERVICE_INFO_PLIST_PATH = './ios/NotificationService/Info.plist';
 
 // Configure ios
 const setupiOS = ({ config }) => {
 	// Get current Info.plist
 	const rocketChatInfo = plist.parse(fs.readFileSync(ROCKET_CHAT_INFO_PLIST_PATH, 'utf-8'));
 	const shareRocketChatInfo = plist.parse(fs.readFileSync(SHARE_ROCKET_CHAT_INFO_PLIST_PATH, 'utf-8'));
+	const notificationServiceInfo = plist.parse(fs.readSync(SHARE_ROCKET_CHAT_INFO_PLIST_PATH, 'utf-8'));
 
 	// Update rocket chat info details
 	rocketChatInfo.CFBundleDisplayName = config.display_name;
@@ -19,6 +21,10 @@ const setupiOS = ({ config }) => {
 	// Update share rocket chat info details
 	shareRocketChatInfo.CFBundleDisplayName = `${config.display_name} Experimental`;
 	shareRocketChatInfo.CFBundleIdentifier = `${config.ios.bundle_identifier_prefix}.${config.company_name}.ShareExtension`;
+
+	// Update notification service info details
+	notificationServiceInfo.CFBundleDisplayName = `${config.display_name} Notification Service`;
+	notificationServiceInfo.CFBundleIdentifier = `${config.ios.bundle_identifier_prefix}.${config.company_name}.NotificationService`;
 
 	// Update bugsnag api
 	rocketChatInfo.bugsnag = {
@@ -34,6 +40,7 @@ const setupiOS = ({ config }) => {
 	// Save file
 	fs.writeFileSync(ROCKET_CHAT_INFO_PLIST_PATH, plist.build(rocketChatInfo));
 	fs.writeFileSync(SHARE_ROCKET_CHAT_INFO_PLIST_PATH, plist.build(shareRocketChatInfo));
+	fs.writeFileSync(NOTIFICATION_SERVICE_INFO_PLIST_PATH, plist.build(notificationServiceInfo));
 };
 
 // Setup both platforms
